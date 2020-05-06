a6999f58eb7bf72a6f541107c42755f785e7dfbd
deock <irving.cruz@students.salle.url.edu>
2020-05-06 17:34:41 +0200

    Added ColorBuilder class

---
+++ src/Module/Color/Application/ColorBuilder.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+
+namespace LaSalle\ChupiProject\Module\Color\Application;
+
+
+use LaSalle\ChupiProject\Module\Color\Domain\Color;
+
+final class ColorBuilder
+{
+    private $colorString;
+
+    public function __toString(): string
+    {
+        return $this->colorString;
+    }
+
+    public function build(): Color
+    {
+        return new Color($this->colorString);
+    }
+
+    public function random(array $colorsArray): self
+    {
+        $this->colorString = $colorsArray[mt_rand(0, count($colorsArray) - 1)];
+        return $this;
+    }
+}

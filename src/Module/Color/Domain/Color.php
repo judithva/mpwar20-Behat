ddec316d3227278cfbe73f82cf1d2c6676655753
deock <irving.cruz@students.salle.url.edu>
2020-05-06 17:35:10 +0200

    Added Color class

---
+++ src/Module/Color/Domain/Color.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace LaSalle\ChupiProject\Module\Color\Domain;
+
+
+final class Color
+{
+    private $color;
+
+    public function __construct(string $color)
+    {
+        $this->color = $color;
+    }
+
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+}

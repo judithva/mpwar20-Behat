38b0fe7e55c8e54ac65f2cc55113c15e1cd63fd3
Judith <judithzenaida.vilela@students.salle.url.edu>
2020-06-01 21:41:19 +0200

    Declare and implementing get method

--- src/Module/Color/Domain/ColorRepository.php
+++ src/Module/Color/Domain/ColorRepository.php
@@ -3,7 +3,11 @@ declare(strict_types=1);
 
 namespace LaSalle\ChupiProject\Module\Color\Domain;
 
+use Colors\Color;
+
 interface  ColorRepository
 {
     public function all(): array;
+
+    public function get(array $colors): Color;
 }
--- src/Module/Color/Infrastructure/InMemoryColorRepository.php
+++ src/Module/Color/Infrastructure/InMemoryColorRepository.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace LaSalle\ChupiProject\Module\Color\Infrastructure;
 
+use Colors\Color;
 use LaSalle\ChupiProject\Module\Color\Domain\ColorRepository;
 
 final class InMemoryColorRepository implements ColorRepository
@@ -10,5 +11,10 @@ final class InMemoryColorRepository implements ColorRepository
     public function all(): array
     {
         return ['red', 'cyan', 'magenta', 'green', 'black', 'yellow', 'blue', 'light_gray'];
+    }
+
+    public function get(array $colors): Color
+    {
+        return new Color($colors[mt_rand(0, count($colors) - 1)]);
     }
 }
